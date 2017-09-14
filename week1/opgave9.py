2a0e7479cdae315069835b3d808b7507ea1550e3
redvox27 <v.t.luder@st.hanze.nl>
2017-09-14 16:23:12 +0200

    opgave 9 klaar

--- week1/opgave9.py
+++ week1/opgave9.py
@@ -1,18 +1,50 @@
-import re
+# in deze lijst komen de uiteindelijke genen
+geneList = []
+
+
 def opgave9():
     sequence = "ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCCCTGCCCCTGGAGGGTGGCCCCACCGGCCGAGACAGCGAGCATATGCAGGAAGCGGCAGGAATAAGGAAAAGCAGCCTCCTGACTTTCCTCGCTTGGTGGTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGGAAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGCAATCCGCGCGCCGGGACAGAATGCCCTGCAGGAACTTCTTCTGGAAGACCTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAGTTTAATTACAGACCTGAA"
 
     #in list L komt de sequence string gesplit op ATG
-    L = []
-    geneList = []
-    L = re.split("ATG| TAG | TAA | TGA", sequence)
-    print(L)
+    L = sequence.split("ATG")
 
-    #check of een string eindigd op ATG, TAG, TAA of TGA
-    #de laatste drie moeten ATG, TAG, TAA of TGA zijn. Dus pak een string uit de lijst en kijk of string[:-3] een van die vier bevat
+    #in deze lijst komen de strings die eindigen op ATG TAG TAA of TGA
+    candidateList = []
 
-    #daarna moet ik kijken of ATG TAG TAA of TGa niet in de string zelf zitten
+    string = ""
 
+    for testSequence in L:
+        print()
+        i = 0
+
+        while i < len(testSequence):
+
+            check = testSequence[i:i+3]
+            if check == "TAA" or check == "ATG" or check == "TAG" or check == "TGA":
+                print(string)
+                candidateList.append(string)
+                string = ""
+
+            print("check", check)
+            string += testSequence[i]
+            print("string: ", string)
+            i += 1
+
+        print("candidateList", candidateList)
+
+    removeFaultySequence(candidateList)
+    print(candidateList)
+    fillGeneList(candidateList)
+    print(geneList)
     #check of de string een veelvoud is van drie
 
+def removeFaultySequence(candidateList):
+    for sequence in candidateList:
+        if "TGA" or "TAG" or "ATG" or "TAA" in sequence:
+            candidateList.remove(sequence)
+
+def fillGeneList(candidateList):
+    for sequence in candidateList:
+        if len(sequence) % 3 == 0:
+            geneList.append(sequence)
 opgave9()
