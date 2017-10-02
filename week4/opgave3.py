89c5ebdf66de6c3a9736999e22e45b07a3bce5a7
redvox27 <v.t.luder@st.hanze.nl>
2017-10-02 14:08:40 +0200

    begin aan week3

---
+++ week4/opgave3.py
@@ -0,0 +1,70 @@
+from tkinter import *
+import tkinter.messagebox
+from tkinter.filedialog import askopenfilename
+from collections import Counter
+
+def show_result():
+    analyze_file(filename.get())
+
+
+def analyze_file(filename):
+    try:
+        infile = open(filename, "r")
+        counts = Counter()
+        wordArray = []
+        frequencyArray = []
+
+        for word in infile.read().split():
+            if len(word) > 1:
+                counts[word] +=1
+        # your code
+        print(counts)
+        for value in sorted(counts.values(), reverse=True):
+            print(counts.values(value))
+
+        #counts = {0: 3, 1: 7, 2: 5}
+
+        # display a histogram for counts
+        #show_histogram(counts)
+    except IOError:
+        tkinter.messagebox.showwarning("Analyze File", "File " + filename + " does not exist")
+
+
+def open_file():
+    filenameforReading = askopenfilename()
+    filename.set(filenameforReading)
+
+
+def show_histogram(counts):
+    numberOfBars = len(counts)
+    width = int(canvas["width"])
+    height = int(canvas["height"])
+    heightBar = 0.75 * height
+    widthBar = width - 20
+    maxCounts = max(counts)
+
+    for i in range(numberOfBars):
+        canvas.create_rectangle(i * widthBar / numberOfBars + 10, height - 20 - counts[i] * heightBar / maxCounts,
+                                (i + 1) * widthBar / numberOfBars + 10, height - 20)
+        canvas.create_text(i * widthBar / numberOfBars + 10 + 0.5 * widthBar / numberOfBars, height - 10,
+                           text=chr(i + ord('a')))
+
+
+window = Tk()
+window.title("Words Frequency Histogram")
+
+frame1 = Frame(window)
+frame1.pack()
+canvas = Canvas(frame1, width=500, height=200)
+canvas.pack()
+
+frame2 = Frame(window)
+frame2.pack()
+
+Label(frame2, text="Enter a filename: ").pack(side=LEFT)
+filename = StringVar()
+Entry(frame2, width=40, textvariable=filename).pack(side=LEFT)
+Button(frame2, text="Browse", command=open_file).pack(side=LEFT)
+Button(frame2, text="Show Result", command=show_result).pack(side=LEFT)
+
+window.mainloop()
